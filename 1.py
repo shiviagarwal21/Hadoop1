d9e0428ca3c6c2f3f4e113d0bc47489de70f2b49
shiviagarwal21 <46994484+shiviagarwal21@users.noreply.github.com>
2020-07-26 01:39:28 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,13 +1,13 @@
-text = open("paragraph.txt", "r") 
-d = dict() 
-for line in text: 
-    line = line.strip() 
-    line = line.lower() 
-    words = line.split(" ") 
-    for word in words: 
+text = open("paragraph.txt", "r") //Open the text file in reading mode
+d = dict()                        // Create a dictionary
+for line in text:                 // Loop through the text file
+    line = line.strip()           // Remove the extra spaces
+    line = line.lower()           // Convert whole text file to lowercase
+    words = line.split(" ")       //Split the words
+    for word in words:            // Loop through words
         if word in d: 
-            d[word] = d[word] + 1
+            d[word] = d[word] + 1   
         else: 
             d[word] = 1
 for key in list(d.keys()): 
-    print(key, ":", d[key])
+    print("Count of ",key, ":", d[key])         // Print the word with count
